2aa8e6a504e10d565eb24a384d4388f74541709a
jackpot <41373877+jakcpto@users.noreply.github.com>
2018-07-19 22:21:51 +0400

    First test of Google map

--- 7.php
+++ 7.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include "header.php";
 include "db.php";
 
@@ -12,25 +12,25 @@ include "db.php";
 <script>
 $(document).ready(function() {
 
-$( "#cntr" ).keyup( function () { 
+$( "#cntr" ).keyup( function () {
 
 var countr = $( "#cntr" ).val();
 var page = $( "#nonopage" ).val();
 
 var req = $.ajax({
   url: "7_back.php",
-  data: { 
+  data: {
         'country': countr,
 	'page'	 : page
     	},
   	type: "POST",
-	success: function(data){ 
+	success: function(data){
         //  echo(data);
 	},
 	error: function(data) {
 	//        alert(data);
 	}
-    
+
  });
 
  req.done(function (response, textStatus, jqXHR){
@@ -49,18 +49,18 @@ var page = $( this ).val();
 
 var req = $.ajax({
   url: "7_back.php",
-  data: { 
+  data: {
         'country': countr,
 	'page'	 : page
     	},
   	type: "POST",
-	success: function(data){ 
+	success: function(data){
         //  echo(data);
 	},
 	error: function(data) {
 	//        alert(data);
 	}
-    
+
  });
  req.done(function (response, textStatus, jqXHR){
           // show successfully for submit message
@@ -72,22 +72,27 @@ var req = $.ajax({
 </script>
 
 <div id="map_canvas"></div>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script function initialize() {     
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBw9ewl5Vwd5I2fy1R1t6wQ5GiYg6xTlQ0"></script>
+<script
+
+// init map
+function initialize() {
 	var myLatlng = new google.maps.LatLng(-34.397, 150.644);
 	var myOptions = {
 		zoom: 8,
 		center: myLatlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 };
 
 var marker = new google.maps.Marker({
 	position: myLatlng,
 	map: map,
-	title:"Hello World!" 
+	title:"Hello World!"
 });
+
+google.maps.event.addDomListener(window, "load", initialize);
 
 </script>
 
