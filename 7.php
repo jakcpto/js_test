90eee2df33d282967653d218ea9602761c512a84
jackpot <41373877+jakcpto@users.noreply.github.com>
2018-07-20 00:34:05 +0400

    Maps shows, but markers not
    maps almost done

--- 7.php
+++ 7.php
@@ -1,5 +1,5 @@
 <?php
-include "header.php";
+include "header7.php";
 include "db.php";
 
 ?>
@@ -12,6 +12,7 @@ include "db.php";
 <script>
 $(document).ready(function() {
 
+// search input
 $( "#cntr" ).keyup( function () {
 
 var countr = $( "#cntr" ).val();
@@ -38,9 +39,52 @@ var req = $.ajax({
           $("#cities").html(response);
      });
 
+// req for maps
+var req2 = $.ajax({
+  url: "7_back_for_map.php",
+  data: {
+        'country': countr,
+	'page'	 : page
+    	},
+  	type: "POST",
+	success: function(data){
+        //  echo(data);
+	},
+	error: function(data) {
+	//        alert(data);
+	}
+
+ });
+
+ req2.done(function (response, textStatus, jqXHR){
+	// clear map
+	//clear_Map();
+	deleteOverlays(); // попробовать обе функции
+          // show successfully for submit message
+     var c_arr = response.split("/");
+	c_arr.forEach(function(item,i,arr){
+		if ( item.length > 0 ) {
+		// alert(item);
+		// вычислить location из названия (элемента массива), кроме пустого
+		    geocoder.geocode( { 'address': item}, function(results, status) {
+      			if (status == 'OK') {
+		        var marker = new google.maps.Marker({
+		            map: map,
+		            position: results[0].geometry.location
+		        });
+		      } else {alert("Fu")} ;
+		}); // geocode
+		// addMarker(location);
+		}; // пустой item
+		});
+          // $("#cities").html(response);
+     }); // req for markers from input
+
+
+
 });
 
-// buttons
+// buttons  for paging
 // debug + test
 $('#cities').on('click', '#button_1', function() {
 
@@ -66,34 +110,73 @@ var req = $.ajax({
           // show successfully for submit message
           $("#cities").html(response);
      });
+
+// req for maps from buttons
+// req for maps
+var req2 = $.ajax({
+  url: "7_back_for_map.php",
+  data: {
+        'country': countr,
+	'page'	 : page
+    	},
+  	type: "POST",
+	success: function(data){
+        //  echo(data);
+	},
+	error: function(data) {
+	//        alert(data);
+	}
+
+ });
+
+ req2.done(function (response, textStatus, jqXHR){
+	// clear map
+	//clear_Map();
+	deleteOverlays(); // попробовать обе функции
+          // show successfully for submit message
+     var c_arr = response.split("/");
+	c_arr.forEach(function(item,i,arr){
+		if ( item.length > 0 ) {
+		// alert(item);
+		// вычислить location из названия (элемента массива), кроме пустого
+		    geocoder.geocode( { 'address': item}, function(results, status) {
+      			if (status == 'OK') {
+		        var marker = new google.maps.Marker({
+		            map: map,
+		            position: results[0].geometry.location
+		        });
+		      };
+		}); // geocode
+		// addMarker(location);
+		}; // пустой item
+		});
+          // $("#cities").html(response);
+     }); //markers
+
 });
 
 }); // document ready
 </script>
 
-<div id="map_canvas"></div>
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBw9ewl5Vwd5I2fy1R1t6wQ5GiYg6xTlQ0"></script>
+<div id="map" class="map"></div>
+
 <script
 
-// init map
-function initialize() {
-	var myLatlng = new google.maps.LatLng(-34.397, 150.644);
-	var myOptions = {
-		zoom: 8,
-		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-};
+// initMap() - функция инициализации карты
 
-var marker = new google.maps.Marker({
-	position: myLatlng,
-	map: map,
-	title:"Hello World!"
-});
-
-google.maps.event.addDomListener(window, "load", initialize);
-
+function initMap() {
+	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
+	var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+	// Обязательные опции с которыми будет проинициализированна карта
+	var mapOptions = {
+		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+		zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+	};
+	// Создаем карту внутри элемента #map
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);	
+}
+// Ждем полной загрузки страницы, после этого запускаем initMap()
+google.maps.event.addDomListener(window, "load", initMap);
 </script>
 
 
